a822e3966da011554697e77c89b0fe7085f58472
netos23 <nikmoroz2002@outlook.com>
2023-06-04 13:41:56 +0300

    upgrade task5

---
+++ buildings/buildings/Cafe.cs
@@ -0,0 +1,40 @@
+﻿namespace buildings
+{
+    public class Cafe : PublicBuilding
+    {
+        public int Capacity { get; }
+
+        public int MishlenStar { get; }
+
+
+        public Cafe(
+            int cost,
+            int level,
+            double strength,
+            Location location,
+            string address,
+            string fiasId,
+            int capacity,
+            int mishlenStar
+        ) : base(cost, level, strength, location, address, fiasId)
+        {
+            Capacity = capacity;
+            MishlenStar = mishlenStar;
+        }
+
+        public override string Visit()
+        {
+            return $"Super cafe with {MishlenStar} mishlen star visited";
+        }
+
+        public string Rate(int rating)
+        {
+            return rating > MishlenStar ? "You are fool? We are the champions" : "God bless you! We are so happy";
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Capacity: {Capacity}, MishlenStar: {MishlenStar}";
+        }
+    }
+}
---
+++ buildings/buildings/Court.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace buildings
+{
+    public class Court : PublicBuilding
+    {
+        public double Luck
+        {
+            get => _innocent / _sentences;
+        }
+
+        public double JudgeName { get; }
+
+        public Court(int cost, int level, double strength, Location location, string address, string fiasId,
+            double judgeName) : base(cost, level, strength, location, address, fiasId)
+        {
+            JudgeName = judgeName;
+            _innocent = 0;
+            _sentences = 0;
+        }
+
+        private double _innocent;
+        private double _sentences;
+
+        public override string Visit()
+        {
+            return "All animals are equal, but some animals are more equal than others. Court visited";
+        }
+
+
+        public string Judge(string humanName)
+        {
+            _sentences++;
+            var random = new Random();
+            if (random.NextDouble() > 0.7)
+            {
+                return "Themis is not favorable, the head off the shoulders!";
+            }
+
+            _innocent++;
+            return "You are innocent";
+        }
+
+        public string Pray(string humanName)
+        {
+            return "Are you serious? Its court mother fucker";
+        }
+    }
+}
